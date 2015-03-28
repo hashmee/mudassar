361d6f053ddefaa4396b0b915f003818edb91636
hashmee <hashmee.smf@gmail.com>
2015-03-28 19:50:18 +0530

    changed the color and removed subheader

--- app/overrides/update_header.rb
+++ app/overrides/update_header.rb
@@ -2,5 +2,5 @@ Deface::Override.new(:virtual_path => 'spree/shared/_header',
 	:name => 'add_phone_to_header',
 	:insert_after => "figure#logo",
 	:text => "<div class='header_phone'>
-     <p>Mudassar:9985169119</p>
+     <p></p>
                </div>")
